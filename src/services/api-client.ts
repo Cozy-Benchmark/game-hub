f874545afbfc7628d375fb5615add8bc8fa69de2
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-07-23 17:37:01 +0800

    Added a .env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+// const gameKey = process.env.REACT_APP_API_KEY;
+
 // TO BE IMPROVED BY ADDING MORE SECURITY 
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
-    params:{
-        key: '5e2906a2c7984b6a905690c5865c2a62'
+    params: {
+        key: '5e2906a2c7984b6a905690c5865c2a62',
     },
-})
+});
