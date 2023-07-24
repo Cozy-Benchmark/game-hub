0f666c0d05ec3cf2d1ea81dd6e3f98febc636593
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-07-25 02:14:28 +0800

    Showed Loading Skeletons

--- src/Components/GameGrid/GameGrid.tsx
+++ src/Components/GameGrid/GameGrid.tsx
@@ -1,9 +1,11 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../../hooks/useGames";
 import GameCard from "../GameCard/GameCard";
+import GameCardSkeleton from "../GameCardSkeleton/GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -13,6 +15,8 @@ const GameGrid = () => {
         spacing={3}
         padding={10}
       >
+        {isLoading &&
+          skeletons.map((skeletons) => <GameCardSkeleton key={skeletons} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
