782a4f89f129b452cbd4af11151b907b50abedd3
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-08-01 16:51:01 +0800

    Refactored Game Grid

--- src/Components/GameGrid/GameGrid.tsx
+++ src/Components/GameGrid/GameGrid.tsx
@@ -6,16 +6,17 @@ import GameCardContainer from "../GameCardContainer/GameCardContainer";
 import { GameQuery } from "../../App";
 
 interface GameGridProp {
-  gameQuery: GameQuery
+  gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: GameGridProp) => {
   const { data, error, isLoading } = useGames(gameQuery); // passes selected genre to games hook
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={6}
